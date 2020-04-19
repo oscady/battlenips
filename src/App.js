418a632d8c0606ac08b823af58bb3dd4fede765f
oscar brady <ob1ddon@hotmail.com>
2020-04-19 22:24:15 +0100

    viewport fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Body from './Components/Body/Body';
-function App() {
-  return (
-    <div className="App">
-      <Body />
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount = () => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Body />
+      </div>
+    );
+  }
 }
 
 export default App;
