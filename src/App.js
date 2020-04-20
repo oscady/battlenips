9742c2e1d7fa45d7f360afcddfe5d74601adfa80
oscar brady <ob1ddon@hotmail.com>
2020-04-20 12:25:28 +0100

    moved screen size event listener to app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import Body from './Components/Body/Body';
 class App extends Component {
 
+  componentDidMount() {
+    window.addEventListener('resize', () => {
+
+      let vh = window.innerHeight * 0.01;
+
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
   render() {
     return (
       <div className="App">
